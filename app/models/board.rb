30432253da96adaceb242e5d02bdc921778d5402
MakMemEng <78347713+MakMemEng@users.noreply.github.com>
2021-08-16 11:37:11 +0900

    Annotate gemを追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  body       :text(65535)
+#  name       :string(255)
+#  title      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Board < ApplicationRecord
 end
